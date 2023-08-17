ec54beb6d641737eec938e86719694c788116340
tokaretin <tokaretin@gmail.com>
2023-08-17 14:35:54 +0200

    Добавьте файлы проекта.

---
+++ 15-3-Gallon/15-3-Gallon.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> vec = { 7,1,5,4,6,3 };
+    int pr = 0;
+
+    for (int i = 1; i < vec.size(); i++)
+    {
+        if (vec[i] - vec[i - 1] > 0)
+            pr += vec[i] - vec[i - 1];
+    }
+
+    std::cout << pr;
+    return 0;
+}
+
